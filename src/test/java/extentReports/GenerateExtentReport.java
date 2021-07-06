31d83d745321a33125a60461b83c520243d36ef1
mwickramanayaka <manulwick@gmail.com>
2021-07-06 05:58:40 +0530

    screenshots added

--- src/test/java/extentReports/GenerateExtentReport.java
+++ src/test/java/extentReports/GenerateExtentReport.java
@@ -1,9 +1,15 @@
 package extentReports;
 
+import java.io.File;
 import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
+import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
@@ -31,30 +37,29 @@ public class GenerateExtentReport {
 	//create the htmlReporter object 
 	ExtentSparkReporter htmlReporter;
 	ExtentReports extent;
-	ExtentTest test1,test2,test3;
+	ExtentTest test;
+	private Object extentReports;
 
 	@BeforeSuite
 	public void setup() throws IOException {
-		
+
 		htmlReporter = new ExtentSparkReporter(System.getProperty("user.dir")+"/test-output/extentReport.html");
-		
-		extent = new ExtentReports();
-		
-		extent.attachReporter(htmlReporter);
-		extent.setSystemInfo("Host name","localhost");
-		extent.setSystemInfo("Environemnt","QA");
-		extent.setSystemInfo("user","manul");
-		
 		htmlReporter.loadXMLConfig(System.getProperty("user.dir")+ "/extent-config.xml");
-		
+
+		//create ExtentReports and attach reporter(s)
+		extent = new ExtentReports();
+		extent.attachReporter(htmlReporter);
+
+		extent.setSystemInfo("Host name","localhost");
+		extent.setSystemInfo("OS","Windows10");
+		extent.setSystemInfo("Environemnt","QA");
+		extent.setSystemInfo("Browser","chrome");
+		extent.setSystemInfo("user","manul");
+
 		/*
 			htmlReporter = new ExtentSparkReporter("extentReport.html");
-
-			//create ExtentReports and attach reporter(s)
-			extent = new ExtentReports();
-			extent.attachReporter(htmlReporter);
 		 */
-		
+
 		//initializing and starting the browser
 		WebDriverManager.chromedriver().setup();
 		driver = new ChromeDriver();
@@ -65,30 +70,32 @@ public class GenerateExtentReport {
 	public void test1() throws InterruptedException {
 
 		//creates a toggle for the given test, add all log events under it
-		test1 = extent.createTest("ebay Search Test", "test to validate search box ");
+		test = extent.createTest("ebay Search Test", "test to validate search box ");
 
-		test1.log(Status.INFO, "Starting test case");
+		test.log(Status.INFO, "Starting test case");
 
 		//maximize the window 
 		driver.manage().window().maximize();
-		test1.pass("maximize has done");
+		test.pass("maximize has done");
 
 		//Navigate to Ebay.com
 		driver.get("https://www.ebay.com");
 		Thread.sleep(1000);
-		test1.pass("Navigate to Ebay.com");
+		test.pass("Navigate to Ebay.com");
 
 		//compare whether the title id matching
 		String actualTitle = driver.getTitle();
 		Assert.assertEquals(actualTitle, expectedTitleebay);
-		test1.pass("title is correct");
+		test.pass("title is correct");
 
 		//enter in the TextBox
 		driver.findElement(By.xpath("//*[@id=\"gh-ac\"]")).sendKeys("Mobile");
-		test1.pass("Entered text in the text box");
+		test.pass("Entered text in the text box");
 		//hit enter
 		driver.findElement(By.xpath("//*[@id=\"gh-btn\"]")).sendKeys(Keys.RETURN);	
-		test1.pass("Press keybopard enter key");
+		test.pass("Press keybopard enter key");
+
+		test.info("test completed");
 
 	}
 
@@ -96,51 +103,86 @@ public class GenerateExtentReport {
 	public void test2() throws InterruptedException {
 
 		//creates a toggle for the given test, add all log events under it
-		test2 = extent.createTest("Daraz Search Test", "test to validate search box ");
+		test = extent.createTest("Daraz Search Test", "test to validate search box ");
 
-		test2.log(Status.INFO, "Starting test case");
+		test.log(Status.INFO, "Starting test case");
 
 		//maximize the window 
 		driver.manage().window().maximize();
-		test2.pass("maximize has done");
+		test.pass("maximize has done");
 
 		//Navigate to daraz.lk
 		driver.get("https://www.daraz.lk/#");
 		Thread.sleep(1000);
-		test2.pass("Navigate to Daraz.lk");
+		test.pass("Navigate to Daraz.lk");
 
 		//compare whether the title id matching
 		String actualTitle = driver.getTitle();
 		Assert.assertEquals(actualTitle, expectedTitledaraz);
-		test2.pass("title is correct");
+		test.pass("title is correct");
 
 		//enter in the TextBox
 		driver.findElement(By.xpath("//*[@id=\"q\"]")).sendKeys("Mobile");
-		test2.pass("Entered thex in the text box");
+		test.pass("Entered text in the text box");
 		//hit enter
 		driver.findElement(By.xpath("//*[@id=\"topActionHeader\"]/div/div[2]/div/div[2]/form/div/div[2]/button")).sendKeys(Keys.RETURN);	
-		test2.pass("Press keybopard enter key");
+		test.pass("Press keybopard enter key");
+
+		test.info("test completed");
 
 	}
 
 	@Test (priority = 2)
-	public void test3() {
-		test3 = extent.createTest("Fail Test", "fail test");
-		test3.log(Status.INFO, "Starting test case");
-		Assert.assertTrue(false);
-		test3.log(Status.FAIL, "test failed");
-		
-	}
-	
-	@AfterMethod
-	public void getResult(ITestResult result) {
-		
-		if (result.getStatus()==ITestResult.FAILURE) {
-			
-			test3.log(Status.FAIL, result.getThrowable());
-			
-		}
+	public void test3() throws InterruptedException {
 
+		//creates a toggle for the given test, add all log events under it
+		test = extent.createTest("Daraz Search Test", "Forcefully failed ");
+
+		test.log(Status.INFO, "Starting test case");
+
+		//maximize the window 
+		driver.manage().window().maximize();
+		test.pass("maximize has done");
+
+		//Navigate to daraz.lk
+		driver.get("https://www.daraz.lk/#");
+		Thread.sleep(1000);
+		test.pass("Navigate to Daraz.lk");
+
+		//compare whether the title id matching
+		String actualTitle = driver.getTitle();
+		Assert.assertEquals(actualTitle, expectedTitledaraz);
+		test.pass("title is correct");
+
+		//enter in the TextBox
+		driver.findElement(By.xpath("//*[@id=\"q\"]")).sendKeys("Mobile");
+		test.pass("Entered text in the text box");
+		//hit enter
+		driver.findElement(By.xpath("invalidXPath")).sendKeys(Keys.RETURN);	
+		test.pass("Press keybopard enter key");
+
+		test.info("test completed");
+		//test.fail("test failed");
+
+	}
+
+	@AfterMethod
+	public void getResult(ITestResult result) throws IOException {
+
+		if (result.getStatus()==ITestResult.FAILURE) {
+
+			test.log(Status.FAIL, "Test case Failed : " + result.getName());
+			test.log(Status.FAIL, "Test case Failed : " + result.getThrowable());
+
+			String screenshotPath = GenerateExtentReport.getScreenShot(driver, result.getName());
+			test.addScreenCaptureFromPath(screenshotPath);// adding screen shot
+
+		}else if (result.getStatus() == ITestResult.SKIP) {
+			test.log(Status.SKIP, "Test Case SKIPPED IS " + result.getName());
+		}
+		else if (result.getStatus() == ITestResult.SUCCESS) {
+			test.log(Status.PASS, "Test Case PASSED IS " + result.getName());
+		}
 	}
 
 	@AfterSuite
@@ -148,15 +190,22 @@ public class GenerateExtentReport {
 
 		driver.quit();
 
-		test1.pass("closed the browser");
-		test2.pass("closed the browser");
-
-		test1.info("test completed");
-		test2.info("test completed");
-		test3.fail("test failed");
-
 		//write results into the file
 		extent.flush();
+
+	}
+
+	public static String getScreenShot(WebDriver driver, String screenShotName) throws IOException {
+
+		String dateName = new SimpleDateFormat("yyyyMMddhhmmss").format(new Date());
+		TakesScreenshot ts = (TakesScreenshot) driver;
+		File source = ts.getScreenshotAs(OutputType.FILE);
+
+		//after execution you will see a folder "FaiiedTestsScreenshots" under src folder
+		String destination = System.getProperty("user.dir")+"/Screnshots/" + screenShotName + dateName + ".png";
+		File finalDestination = new File(destination);
+		FileUtils.copyFile(source, finalDestination);
+		return destination;
 
 	}
 
